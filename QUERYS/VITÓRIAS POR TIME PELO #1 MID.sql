5f771e95d9dca17ebbec0687d334c4b40655d366
Weverton <wevertonbezzera013@gmail.com>
2021-01-12 13:22:00 -0300

    Busca de VITÓRIAS POR TIME PELO #1 MID.sql

---
+++ QUERYS/VITÓRIAS POR TIME PELO #1 MID.sql
@@ -0,0 +1,25 @@
+SELECT W.Equipe, W.Vitorias, T.Partidas, ((Vitorias/Partidas)*100) AS Percentual_de_Vitorias FROM
+(SELECT Equipe, COUNT(Equipe) AS Vitorias FROM
+(SELECT * FROM
+(SELECT blueTeamTag AS Equipe, bResult, blueMiddle as Mid
+FROM LeagueOfLegends 
+WHERE bResult = 1 AND (blueMiddle LIKE '%tockers%')) AS Blue
+UNION ALL 
+SELECT * FROM
+(SELECT redTeamTag AS Equipe, rResult, redMiddle
+FROM LeagueOfLegends 
+WHERE rResult = 1 AND (redMiddle LIKE '%tockers%')) AS Red) AS TABELA
+GROUP BY Equipe) AS W
+JOIN
+(SELECT Equipe, COUNT(Equipe) AS Partidas FROM
+(SELECT * FROM
+(SELECT blueTeamTag AS Equipe, bResult, blueMiddle as Mid
+FROM LeagueOfLegends 
+WHERE blueMiddle LIKE '%tockers%') AS Blue
+UNION ALL 
+SELECT * FROM
+(SELECT redTeamTag AS Equipe, rResult, redMiddle
+FROM LeagueOfLegends 
+WHERE redMiddle LIKE '%tockers%') AS Red) AS TABELA
+GROUP BY Equipe) AS T
+ON W.Equipe=T.Equipe;
