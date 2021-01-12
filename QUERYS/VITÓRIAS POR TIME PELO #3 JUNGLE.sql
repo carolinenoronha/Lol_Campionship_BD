16dccac05ed06bc9b548a2d5f0fd5b3cfdc3f9a2
Weverton <wevertonbezzera013@gmail.com>
2021-01-12 13:22:29 -0300

    Create VITÓRIAS POR TIME PELO #3 JUNGLE.sql

---
+++ QUERYS/VITÓRIAS POR TIME PELO #3 JUNGLE.sql
@@ -0,0 +1,25 @@
+SELECT W.Equipe, W.Vitorias, T.Partidas, ((Vitorias/Partidas)*100) AS Percentual_de_Vitorias FROM
+(SELECT Equipe, COUNT(Equipe) AS Vitorias FROM
+(SELECT * FROM
+(SELECT blueTeamTag AS Equipe, bResult, blueJungle as Jungle
+FROM LeagueOfLegends 
+WHERE bResult = 1 AND (blueJungle LIKE '%Ranger%')) AS Blue
+UNION ALL 
+SELECT * FROM
+(SELECT redTeamTag AS Equipe, rResult, redJungle
+FROM LeagueOfLegends 
+WHERE rResult = 1 AND (redJungle LIKE '%Ranger%')) AS Red) AS TABELA
+GROUP BY Equipe) AS W
+JOIN
+(SELECT Equipe, COUNT(Equipe) AS Partidas FROM
+(SELECT * FROM
+(SELECT blueTeamTag AS Equipe, bResult, blueJungle as Jungle
+FROM LeagueOfLegends 
+WHERE blueJungle LIKE '%Ranger%') AS Blue
+UNION ALL 
+SELECT * FROM
+(SELECT redTeamTag AS Equipe, rResult, redJungle
+FROM LeagueOfLegends 
+WHERE redJungle LIKE '%Ranger%') AS Red) AS TABELA
+GROUP BY Equipe) AS T
+ON W.Equipe=T.Equipe;
